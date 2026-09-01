b74718609a194b6c7a0b16e3d98c3ed734dc1be6
Hermes Alba <hermesalba@MacBook-Pro-2.local>
2026-09-01 08:07:13 -0500

    Revert "CaseCard: ocultar fecha en movil solo en el bloque de Home"
    This reverts commit d730890f4754acd864299a46b0dcb95f8dd58040.

--- components/sections/featured-case-studies.tsx
+++ components/sections/featured-case-studies.tsx
@@ -45,7 +45,7 @@ export function FeaturedCaseStudies() {
               delay={index * 100}
               direction={index % 2 === 0 ? "up" : "left"}
             >
-              <CaseCard caseStudy={caseStudy} background="indigo" hideDateOnMobile />
+              <CaseCard caseStudy={caseStudy} background="indigo" />
             </FadeIn>
           ))}
         </div>
